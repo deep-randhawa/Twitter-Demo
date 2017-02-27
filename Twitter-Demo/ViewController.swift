46f8b5407cf0b20ef701342fb20c0ab7fb8660c0
Deep S Randhawa <deepsaranr@gmail.com>
2017-02-26 23:48:12 -0500

    Initial Commit

---
+++ Twitter-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Twitter-Demo
+//
+//  Created by Deep S Randhawa on 2/26/17.
+//  Copyright © 2017 Deep S Randhawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
